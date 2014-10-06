d83a82528b3a1aa1df18eb0936735685d7fe65d8
Carl Pearson <cap10@ufl.edu>
2014-10-06 11:53:31 -0400

    fix 2+ intervals; output fiddling

--- agg.py
+++ agg.py
@@ -5,14 +5,19 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="agg: a command line utility for aggregating lines of output.")
     parser.add_argument("filenames", metavar="N", nargs="*", help="paths to files for aggregation", default=["/dev/stdin"], type=str)
     parser.add_argument("-s", dest="separator", help="separator string", default=" ", type=str)
-    parser.add_argument("-i", dest="interval", help="aggregation interval", default=7, type=int)
     parser.add_argument("-v", dest="verbose", action='store_true', help="verbose mode.")
-    ## parser.add_argument("-r", help="should separator be treated as a regular expression?", default=False, type=bool)
-    ## parser.add_argument("-c", help="do the inputs have a header line?", default=False, type=bool)
+    #parser.add_argument("-w", dest="warnings", action='store_true', help="check for and display warnings.")
+
+    interval_group = parser.add_mutually_exclusive_group()
+    interval_group.add_argument("-i", dest="interval", help="aggregation interval", default=7, type=int)
+    interval_group.add_argument("-d", nargs='+',
+        dest="interval", help="special aggregation interval.", default="week", choices=['week','month','year'])
+    ## parser.add_argument("-r", help="should separator be treated as a regular expression?", action="store_true")
+    parser.add_argument("-c", dest="header", help="do the inputs have a header line?", action="store_true")
     input_args = parser.parse_args()
-    print(input_args)
+    if input_args.verbose:
+        print("using parsing arguments: %s" % input_args)
     interval = input_args.interval
-    data_buffer = []
     agg_fun = sum
 
     def clean_break(line):
@@ -21,26 +26,32 @@ if __name__ == "__main__":
     def output(data_buffer):
         print(input_args.separator.join(map(str, data_buffer)))
 
-    def headline(filename):
+    def headline(filename, header):
         if (input_args.verbose):
-            print(filename)
+            print("file: %s - cols:" % filename)
+            print(" ".join(header))
 
     def parse_head(file):
-        line = file.readline()
         ## TODO parse header info here if option set
-        parts = clean_break(line)
+        parts = clean_break(file.readline())
+        if input_args.header:
+            header = parts
+            parts = clean_break(file.readline())
+        else:
+            header = ["col%d"%i for i in range(len(parts))]
         num_fun = [ (float if p.find('.') != -1 else int) for p in parts]  # look for E as well?
         buffer = [ f(y) for f, y in zip(num_fun, parts)]
-        return buffer, num_fun
+        if input_args.verbose:
+            print("detected number formats: %s" % num_fun)
+        return buffer, num_fun, header
 
     def parse_line(l, buffer, item_parser, agg):
         return [agg([b, nf(p)]) for p, nf, b in zip(clean_break(l), item_parser, buffer)]
 
     for filename in input_args.filenames:
-        headline(filename)
         with open(filename,'r') as f:
-            ## TODO: get the first line, handle it as as special case
-            data_buffer, number_parser = parse_head(f)
+            data_buffer, number_parser, header = parse_head(f)
+            headline(filename, header)
             counter = 1
             for line in f:
                 parts = clean_break(line)
@@ -48,8 +59,9 @@ if __name__ == "__main__":
                 counter += 1
                 if counter == interval:
                     output(data_buffer)
-                    data_buffer = []
+                    data_buffer = [nf() for nf in number_parser]
+                    ## TODO need appropriate init value for aggregation function - e.g., 0 doesn't work w/ prod
                     counter = 0
             if counter != 0:
-                print("partial aggregation:",counter,"lines.")
+                print("partial aggregation on %d lines:" % counter)
                 output(data_buffer)
